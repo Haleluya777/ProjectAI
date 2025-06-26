0f1574b513ea4e88a648b837798b1a2a8ac402f0
Honolulu <88095893+Haleluya777@users.noreply.github.com>
2025-06-26 12:10:46 +0900

    0626

--- Assets/Resources/Scripts/Skills/Skill_Dash.cs
+++ Assets/Resources/Scripts/Skills/Skill_Dash.cs
@@ -8,15 +8,31 @@ public class Skill_Dash : SkillBase
 {
     //스킬이 가지는 고유값.
     [SerializeField] private int dashDistance; //대쉬 거리
-    [SerializeField] private int dashSpeed; //대쉬 속도
+    [SerializeField] private int duration; //목표 도달까지 걸리는 시간
     public override bool UseSkill()
     {
         if (!base.UseSkill()) return false;
         Debug.Log("대쉬 스킬!");
         //대쉬 주요 기믹
-        //caster.GetGameObject().transform.Translate(new Vector3(0,0,0));
-        caster.GetGameObject().transform.position = new Vector3(0, 0, 0);
+        //StartCoroutine(MoveToTarget(new Vector3(0, 0, 0), 0.5f));
         //
         return true;
+    }
+
+    IEnumerator MoveToTarget(Vector3 target, float duration)
+    {
+        float timeElapsed = 0f; // 경과 시간
+        Vector3 startPos = caster.GetGameObject().transform.position; // 코루틴 시작 시점의 위치
+
+        while (timeElapsed < duration)
+        {
+            float t = timeElapsed / duration;
+            caster.GetGameObject().transform.position = Vector3.Lerp(startPos, target, t);
+
+            timeElapsed += Time.deltaTime; // 다음 프레임까지의 시간 증가
+            yield return null; // 다음 프레임까지 대기
+        }
+        caster.GetGameObject().transform.position = target;
+        Debug.Log("이동 완료!");
     }
 }
