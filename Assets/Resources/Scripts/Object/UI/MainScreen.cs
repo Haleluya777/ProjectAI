9307476ff08be4c13bf74ebeef3637d2ed8c96dd
cruesate <hatchannyee@gmail.com>
2025-08-26 20:20:44 +0900

    MainScreen씬 및 UI 스크립트폴더 추가

---
+++ Assets/Resources/Scripts/Object/UI/MainScreen.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainScreen : MonoBehaviour
+{
+    public void NewGame()
+    {
+        SceneManager.LoadScene("SampleScene1");
+    }
+    public void LoadGame()
+    {
+        
+    }
+    public void Settings()
+    {
+        
+    }
+    public void Credits()
+    {
+        
+    }
+    public void QuitGame()
+    {
+        
+    }
+}
--- Assets/Resources/Scripts/Object/UI/SaveUI.cs
+++ Assets/Resources/Scripts/Object/UI/SaveUI.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Spawnpoint : MonoBehaviour, IInteractable
+public class SaveUI : MonoBehaviour, IInteractable
 {
     public void Interact()
     {
